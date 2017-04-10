4c50afa0af5ee0d82e9780cf69864494d7a6a549
Nassif Bourguig <nbourguig@gmail.com>
2017-04-10 16:35:32 +0200

    Fix App namespace declaration

--- src/Directive.php
+++ src/Directive.php
@@ -6,7 +6,7 @@ class Directive
 {
     public function render($key, $value)
     {
-        $var = PHP_EOL . "window.App || window.App = {}; window.App.{$key} = " . json_encode($value, JSON_HEX_TAG) . ';' . PHP_EOL;
+        $var = PHP_EOL . "window.App = window.App || {}; window.App.{$key} = " . json_encode($value, JSON_HEX_TAG) . ';' . PHP_EOL;
 
         return PHP_EOL . "<script>{$var}</script>" . PHP_EOL;
     }
